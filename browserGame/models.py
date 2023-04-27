dd381e5798358e3ec5422bd6fded6607b4ff5453
Andres Villca <andres24vt@gmail.com>
2023-04-27 22:45:52 +0200

    models

--- browserGame/models.py
+++ browserGame/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+# Create your models here.
+class userRols(models.Model):
+    idUser= models.ForeignKey(User, on_delete=models.CASCADE)
+    rold= models.IntegerField()
+
+class players(models.Model):
+    # name= models.ForeignKey(User.objects.filter["name":name], on_delete=models.CASCADE)
+    level= models.IntegerField()
+    life= models.IntegerField()
+    energi= models.IntegerField()
+    experience= models.IntegerField()
+
+
+class globalOptions(models.Model):
+    opc= models.CharField(max_length=100)
+
+class accions():
+    user= models.ForeignKey(players, on_delete=models.CASCADE)
+    typeA= models.CharField(max_length=100)
